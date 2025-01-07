c47f902a39e6a2c0ced43142e185f5d8e1336983
hakam atassi <atassi.hakam@gmail.com>
2025-01-07 17:03:57 -0500

    added multicore_template

---
+++ benchmarks/multicore_template/main.c
@@ -0,0 +1,36 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Median filter bencmark
+//--------------------------------------------------------------------------
+//
+// This benchmark performs a 1D three element median filter. The
+// input data (and reference data) should be generated using the
+// median_gendata.pl perl script and dumped to a file named
+// dataset1.h.
+
+#include "util.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//--------------------------------------------------------------------------
+// Input/Reference Data
+
+
+void thread_entry(int cid, int nc) {
+    if (cid == 0) {
+        printf("Core 0 running\n");
+        while(1);
+    } else if(cid==1) {
+        printf("Core 1 running\n", cid);
+        while(1);
+    }
+    return;
+}
+
+
+int main(int argc, char** argv) {
+    return 0;
+}
