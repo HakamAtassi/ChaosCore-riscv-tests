3dbbd1526857a5f8ab411db6fe8414a449daca96
hakam atassi <atassi.hakam@gmail.com>
2025-03-17 20:58:42 -0400

    render stuff

--- benchmarks/render/render.c
+++ benchmarks/render/render.c
@@ -1,41 +1,31 @@
 #include "mmio.h"
 
-#define GCD_STATUS 0x4000
-#define GCD_X 0x4004
-#define GCD_Y 0x4008
-#define GCD_GCD 0x400C
+#define PPM_STATUS 0x4000
+#define PPM_OPERATION 0x4004
+#define PPM_ADDRESS 0x4008
+#define PPM_DATA 0x400C
+#define PPM_DUMP 0x4010
+#define PPM_DOORBELL 0x4014
 
-unsigned int gcd_ref(unsigned int x, unsigned int y) {
-  while (y != 0) {
-    if (x > y)
-      x = x - y;
-    else
-      y = y - x;
-  }
-  return x;
-}
 
 int main(void)
 {
+
   uint32_t result, ref, x = 20, y = 15;
 
   // wait for peripheral to be ready
-  while ((reg_read8(GCD_STATUS) & 0x2) == 0) ;
+  while (reg_read8(PPM_STATUS) == 0) ;
 
-  reg_write32(GCD_X, x);
-  reg_write32(GCD_Y, y);
+  reg_write32(PPM_DATA, 0xFF);
+  reg_write32(PPM_OPERATION, 1);
 
-
-  // wait for peripheral to complete
-  while ((reg_read8(GCD_STATUS) & 0x1) == 0) ;
-
-  result = reg_read32(GCD_GCD);
-  ref = gcd_ref(x, y);
-
-  if (result != ref) {
-    printf("Hardware result %d does not match reference value %d\n", result, ref);
-    return 1;
+  for(int i=0;i<320*20;i++){
+    reg_write32(PPM_ADDRESS, i);
+    reg_write32(PPM_DOORBELL, 1);
   }
-  printf("Hardware result %d is correct for GCD\n", result);
+
+
+  reg_write32(PPM_DUMP, 1);
+
   return 0;
 }
