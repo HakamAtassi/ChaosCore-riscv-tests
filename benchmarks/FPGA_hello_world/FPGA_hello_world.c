4d5cc17f78659795f2a89c611a069c85ffd0b720
hakam atassi <atassi.hakam@gmail.com>
2025-03-22 22:28:49 -0400

    added hello world

--- benchmarks/FPGA_hello_world/FPGA_hello_world.c
+++ benchmarks/FPGA_hello_world/FPGA_hello_world.c
@@ -4,12 +4,25 @@
 
 #define ULTRASCALE_FPGA
 
+#define UART1_ADDR 0xFF000020
+
+void putch(char c){
+  *(volatile uint8_t *)UART1_ADDR = c;
+}
+
+void UART1_printf(char* str){
+
+  for(int i=0;str[i]!='\0';i++){
+    putch(str[i]);
+  }
+
+}
+
 int main(int argc, char *argv[])
 {
 
-  while(1){
-    *(volatile uint8_t *)0xFF000020 = 0x41;
-  }
+  UART1_printf("Hello, world!");
+
 
   return 0;
 }
